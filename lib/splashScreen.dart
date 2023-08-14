2ea70b9dbc34539eea5a4cf77a9c408be5b2b482
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2023-08-14 10:26:09 +0530

    Initial commit

---
+++ lib/splashScreen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
